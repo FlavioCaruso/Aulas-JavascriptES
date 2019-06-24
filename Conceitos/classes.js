edc720468d39ef1e2d63abbac2bb6fd957be70ec
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-06-24 16:25:36 -0300

    Primeiro commit

---
+++ Conceitos/classes.js
@@ -0,0 +1,42 @@
+// Para ver este script, altere o valor do script "conceitos" no arquivo "package.json"
+/*
+    "conceitos": "babel ./conceitos/classes.js -o ./bundle.js -w",
+*/
+
+// class List {
+//     constructor(){
+//         this.data = [];
+//     }
+
+//     add(data){
+//         this.data.push(data);
+//         console.log(this.data)
+//     }
+// }
+
+// class TodoList extends List{
+//    constructor(){
+//        super();
+
+//        this.usuario = "Flavio";
+//    }
+
+//    mostraUsuario(){
+//        console.log(this.usuario)
+//    }
+// }
+
+// var MinhaLista = new TodoList();
+
+// document.getElementById('novotodo').onclick = function(){
+//     MinhaLista.add('Novo todo');
+// }
+
+// MinhaLista.mostraUsuario();
+
+class Matematica {
+    static soma(a, b){
+        return a + b;
+    }
+}
+console.log(Matematica.soma(1, 2));
