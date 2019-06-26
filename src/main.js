bb4133d480f065bdd9aabd9dc5a394d7c7639200
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-06-26 14:53:19 -0300

    Finalizada a primeira aula de Async/await

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
+/*
+Exercicios do Webpack Server
+
 import ClasseUsuario, { idade as IdadeUsuario } from './functions'
 
-//Default
+//Importando Default
 ClasseUsuario.info();
 
-//Variavel
+//Importando uma Variavel
 console.log(IdadeUsuario);
+*/
+const minhaPromise = () => new Promise((resolve, reject) =>{
+    setTimeout(() => { resolve('OK')}, 2000)
+});
+
+// async function executaPromise(){
+//     Executa a proxima função apenas depois da primeira já ter sido executada
+//     Await só pode ser executado dentro de uma função async
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
+
+//Usando Arrow Function
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+};
+
+executaPromise();
